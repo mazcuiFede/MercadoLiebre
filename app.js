e20e0df4c2a7efddb4fd3524002623b7421632be
Your Name <fernandezgermannicolas@gmail.com>
2021-11-02 23:02:05 -0300

    primera parte

--- app.js
+++ app.js
@@ -10,5 +10,27 @@ app.listen(port, () => {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/home.html"));
+    res.sendFile(path.join(__dirname, "/views/home.html"))});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/ofertas.html"))});  
+
+ app.get("/", (req, res) => {
+     res.sendFile(path.join(__dirname, "/views/tiendasoficiales.html"))});  
+     
+app.get("/", (req, res) => {
+     res.sendFile(path.join(__dirname, "/views/vender.html"))});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/ayuda.html"))});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/creatucuenta.html"))});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/ingresa.html"))});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/miscompras.html"))});
+
 });
