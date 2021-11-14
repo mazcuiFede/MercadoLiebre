4a635a383152193d55440ee19f72ba5efa123314
Matias Azcui <matias.azcui@gmail.com>
2021-11-14 18:18:49 -0300

    Para heroku

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = 3030;
-app.use(express.static("public"));
+process.env.PWD = process.cwd()
+
+app.use(express.static(process.env.PWD + '/public'));
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
@@ -12,24 +15,24 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"))});
 
-app.get("/", (req, res) => {
+app.get("/ofertas", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/ofertas.html"))});  
 
- app.get("/", (req, res) => {
+app.get("/tiendasoficiales", (req, res) => {
      res.sendFile(path.join(__dirname, "/views/tiendasoficiales.html"))});  
      
-app.get("/", (req, res) => {
+app.get("/vender", (req, res) => {
      res.sendFile(path.join(__dirname, "/views/vender.html"))});
 
-app.get("/", (req, res) => {
+app.get("/ayuda", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/ayuda.html"))});
 
-app.get("/", (req, res) => {
+app.get("/creatucuenta", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/creatucuenta.html"))});
 
-app.get("/", (req, res) => {
+app.get("/ingresa", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/ingresa.html"))});
 
-app.get("/", (req, res) => {
+app.get("/miscompras", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/miscompras.html"))});
 
